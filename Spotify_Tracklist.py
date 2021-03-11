401f7a4050b0d6028a7377abdfbee56a7dfa97ea
gunveerv <60611673+gunveerv@users.noreply.github.com>
2021-03-10 22:46:36 -0500

    Update Spotify_Tracklist.py

--- Spotify_Tracklist.py
+++ Spotify_Tracklist.py
@@ -1,7 +1,7 @@
 """
 @Creator Gunveer V,
 Takes a database of spotify tracks and determines whether a set of given attributes will make a track "popular"
-Only takes acousticness so far
+Only takes danceability so far
 """
 
 #beginning
@@ -17,30 +17,29 @@ plt.style.use('bmh')
 data = pd.read_csv("data.csv")
 
 #attributes = ['acousticness', 'danceability', 'energy', 'liveness', 'popularity' , 'speechiness', 'valence']
-attributes = ['acousticness', 'popularity']
+attributes = ['danceability', 'popularity']
 data = data[attributes]
 
 #print(data.head(5))
 
 #creat x and y plots
-x = np.array(data.drop(['acousticness'], 1)) [:1000]
-y = np.array(data.drop(['popularity'], 1)) [:1000]
+x = np.array(data.drop(['popularity'], 1)) [:100000]
+y = np.array(data.drop(['danceability'], 1)) [:100000]
 
 #create linear regression model
 model = LinearRegression()
 model.fit(x, y)
 
-x_new = np.linspace(0, 30, 100)
+x_new = np.linspace(0, 1)
 y_new = model.predict(x_new[:, np.newaxis])
 
 #show data
 # plot the results
-valid =  data[x.shape[0]:]
+
 plt.title('Model')
-plt.xlabel('Acousticness',fontsize=18)
+plt.xlabel('Danceability',fontsize=18)
 plt.ylabel('Popularity',fontsize=18)
 plt.scatter(x,y)
 plt.plot(x_new,y_new, 'k')
 
 plt.show()
-
